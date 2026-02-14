2b84d26f1a3ab26bd6d1b349d47f3698c492a6f7
Karlo M <karlo.marinovic@decode.agency>
2026-02-14 18:58:34 +0100

    feat: render HomeScreen in MainActivity

--- app/src/main/java/com/imkaem/android/svarctoo/MainActivity.kt
+++ app/src/main/java/com/imkaem/android/svarctoo/MainActivity.kt
@@ -5,12 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.imkaem.android.svarctoo.ui.screens.HomeScreen
 import com.imkaem.android.svarctoo.ui.theme.SvarcTooTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +18,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SvarcTooTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
+                    HomeScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainActivityPreview() {
     SvarcTooTheme {
-        Greeting("Android")
+        HomeScreen()
     }
 }
