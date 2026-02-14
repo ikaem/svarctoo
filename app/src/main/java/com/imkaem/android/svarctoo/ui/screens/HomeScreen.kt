2ec6003e05545e7132b3c88f36c2e9426c04686e
Karlo M <karlo.marinovic@decode.agency>
2026-02-14 14:16:01 +0100

    feat: add Compose preview for HomeScreen

--- app/src/main/java/com/imkaem/android/svarctoo/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/imkaem/android/svarctoo/ui/screens/HomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -51,4 +52,10 @@ private fun CurrentMonthSummarySection() {
 @Composable
 private fun LatestExpensesSection() {
     // Placeholder for Section 4
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
 }
